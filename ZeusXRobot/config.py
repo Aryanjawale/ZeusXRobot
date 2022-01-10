1224d8944375636c23ea970f0e0b4b3b50a65f1d
Aryan Jawale <87902607+Aryanjawale@users.noreply.github.com>
2022-01-10 13:53:56 +0530

    Update config.py

--- ZeusXRobot/config.py
+++ ZeusXRobot/config.py
@@ -15,27 +15,27 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 1234567  # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1491497760  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "ctzfamily"
-    SUPPORT_CHAT = 'UnitedSupport'  #Your own group for support, do not add the @
+    API_ID = 4759912  # integer value, dont use ""
+    API_HASH = "5bcc548f108e30a913a339a4f97f315c"
+    TOKEN = "5059068789:AAG1p9zixkDPEUZCjlKOz1g-OjPWaH1Xx0g"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1323719429  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Aryanjawale"
+    SUPPORT_CHAT = 'trainer_zone'  #Your own group for support, do not add the @
     UPDATES_CHANNEL = 'ZeusUpdates' #Your own channel for Updates of bot, Do not add @
-    JOIN_LOGGER = -1001574034400  #Prints any new group the bot is added to, prints just the name and ID.
-    REM_BG_API_KEY = "http://removebg.com"
-    TEMP_DOWNLOAD_DIRECTORY = ""
-    EVENT_LOGS = -1001574034400  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    JOIN_LOGGER = -1001197394366  #Prints any new group the bot is added to, prints just the name and ID.
+    REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    EVENT_LOGS = -1001197394366  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     SQLALCHEMY_DATABASE_URI = '' #do you hub your old heroku app database_URL then put here, most use 25days ago sql
     LOAD = [] #try to kang this db ur big mothersfuckers i know your noob so only kanging my db
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = None
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "M7r11AlyDZGvFh2F9U6umCS96iVvyMC~Bo9khw2cKMcYvRBYyDOUNJ7FLQqEEwgP"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
-    BOT_USERNAME = "ZeusXRobot"
-    BOT_ID = "2116046406"
+    BOT_USERNAME = "HawkMoth_rbot"
+    BOT_ID = "5059068789"
     
     DRAGONS = get_user_list('elevated_users.json', 'sudos')
 
@@ -55,10 +55,10 @@ class Config(object):
     BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     ARQ_API_URL = "https://thearq.tech"
-    ARQ_API_KEY = "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ"
-    CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
-    TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
-    OPENWEATHERMAP_ID = 'awoo'
+    ARQ_API_KEY = "VPGDCM-JFWZOY-OQQHWN-OLIIMI-ARQ"
+    CASH_API_KEY = '24IWG7JGXMR46QEE'  # Get your API key from https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = 'YGTK7XPATUHK'  # Get your API key from https://timezonedb.com/api
+    OPENWEATHERMAP_ID = 'e8c43576833a8867d24a4e6785349e20'
     WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
     AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
